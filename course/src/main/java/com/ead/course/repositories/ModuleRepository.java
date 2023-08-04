ea846e5cb4a7205dcaafb4bfb37b572ed8d05891
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-08-04 11:57:55 -0300

    FetchType Eager/Lazy, @EntityGraph, FetchMode SELECT/SUBSELECT/JOIN, @Query e @Modify

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -2,8 +2,20 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+
+    @Query(value = "select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
+
+    //in case to use update or delete actions, use @Modifying along with @Query
+
+//    @EntityGraph(attributePaths = {"course"})
+//    ModuleModel findByTitle(String title);
 }
