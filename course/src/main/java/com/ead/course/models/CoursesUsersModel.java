e5b52f02dd93fe28e709df0255d7cbe239ec9ebf
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-08-21 15:08:30 -0300

    Change CoursesUsersModel attributes

--- course/src/main/java/com/ead/course/models/CoursesUsersModel.java
+++ course/src/main/java/com/ead/course/models/CoursesUsersModel.java
@@ -4,10 +4,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.kaczmarzyk.spring.data.jpa.web.annotation.Join;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.UUID;
 
@@ -16,10 +22,9 @@ import java.util.UUID;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Entity
-@Table(name = "TB_COURSES_USER")
+@Table(name = "COURSES_USER")
 @IdClass(CoursesUsersModel.class)
 public class CoursesUsersModel implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -31,10 +36,7 @@ public class CoursesUsersModel implements Serializable {
     @Type(type = "uuid-char")
     private UUID userId;
 
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false, cascade = CascadeType.ALL)
     @JoinColumn(name = "course_id", referencedColumnName = "courseId")
     @Type(type = "uuid-char")
-    private CourseModel course;
-
+    private UUID courseId;
 }
