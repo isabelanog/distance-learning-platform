7204fc317df927ccbfa535e001aae78d0f7fcd65
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-08-22 15:02:50 -0300

    Adapting existsByCourseIdAndAndUserId in service and create addUserToCourse method

--- authuser/src/main/java/com/ead/authuser/service/impl/UserCoursesServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/service/impl/UserCoursesServiceImpl.java
@@ -1,13 +1,26 @@
 package com.ead.authuser.service.impl;
 
+import com.ead.authuser.models.UserCoursesModel;
 import com.ead.authuser.repositories.UserCoursesRepository;
 import com.ead.authuser.service.UserCoursesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserCoursesServiceImpl implements UserCoursesService {
 
     @Autowired
     UserCoursesRepository userCoursesRepository;
+
+    @Override
+    public boolean isUserSubscribedToCourse(UUID courseId, UUID userId) {
+        return userCoursesRepository.existsByCourseIdAndAndUserId(courseId, userId);
+    }
+
+    @Override
+    public UserCoursesModel addUserToCourse(UserCoursesModel userCoursesModel) {
+        return userCoursesRepository.save(userCoursesModel);
+    }
 }
