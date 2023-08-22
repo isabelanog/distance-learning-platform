d1f10e4f1cf093ab42db712e6bdcc7e4a4e67689
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-08-22 15:02:06 -0300

    Adapting existsByCourseIdAndAndUserId method

--- authuser/src/main/java/com/ead/authuser/repositories/UserCoursesRepository.java
+++ authuser/src/main/java/com/ead/authuser/repositories/UserCoursesRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UserCoursesRepository extends JpaRepository<UserCoursesModel, UUID> {
-
+    boolean existsByCourseIdAndAndUserId(UUID courseId, UUID userId);
 }
