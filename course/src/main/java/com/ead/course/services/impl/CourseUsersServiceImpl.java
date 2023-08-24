cb82b2ec032bf8c17a7c66aa2c65284f84162554
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-08-24 17:49:48 -0300

    Add saveAndSendUserSubscriptionInCourseToAuthUserMicroservice impl + fix methods

--- course/src/main/java/com/ead/course/services/impl/CourseUsersServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseUsersServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.clients.AuthUserClient;
 import com.ead.course.models.CourseModel;
 import com.ead.course.models.CoursesUsersModel;
 import com.ead.course.repositories.CourseUsersRepository;
 import com.ead.course.services.CourseUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -15,13 +17,28 @@ public class CourseUsersServiceImpl implements CourseUsersService {
     @Autowired
     CourseUsersRepository courseUsersRepository;
 
+    @Autowired
+    AuthUserClient authUserClient;
+
     @Override
     public boolean isUserSubscribedToCourse(CourseModel course, UUID userId) {
-        return courseUsersRepository.existsByCourseIdAndUserId(course.getCourseId(), userId);
+        return courseUsersRepository.existsByCourseAndUserId(course, userId);
     }
 
     @Override
-    public void addUserToCourse(CoursesUsersModel coursesUsersModel) {
-        courseUsersRepository.save(coursesUsersModel);
+    public CoursesUsersModel addUserToCourse(CoursesUsersModel coursesUsersModel) {
+        return courseUsersRepository.save(coursesUsersModel);
+    }
+    @Transactional
+    @Override
+    public CoursesUsersModel saveAndSendUserSubscriptionInCourseToAuthUserMicroservice(CoursesUsersModel coursesUsersModel) {
+
+        UUID userId = coursesUsersModel.getUserId();
+        UUID courseId = coursesUsersModel.getCourse().getCourseId();
+
+        coursesUsersModel = courseUsersRepository.save(coursesUsersModel);
+
+        authUserClient.postSubscriptionUserInCourse(courseId, userId);
+        return coursesUsersModel;
     }
 }
