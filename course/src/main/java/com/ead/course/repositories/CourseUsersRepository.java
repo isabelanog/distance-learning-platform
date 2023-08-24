be686192db812bd74ba83ba7627cba81c7b9523f
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-08-24 17:48:56 -0300

    Rollback to CourseModel parameter

--- course/src/main/java/com/ead/course/repositories/CourseUsersRepository.java
+++ course/src/main/java/com/ead/course/repositories/CourseUsersRepository.java
@@ -1,10 +1,11 @@
 package com.ead.course.repositories;
 
+import com.ead.course.models.CourseModel;
 import com.ead.course.models.CoursesUsersModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface CourseUsersRepository extends JpaRepository<CoursesUsersModel, UUID> {
-    boolean existsByCourseIdAndUserId(UUID courseId, UUID userId);
+    boolean existsByCourseAndUserId(CourseModel courseModel, UUID userId);
 }
