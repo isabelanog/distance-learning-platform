f3e73ce4e1ec76b3ee815b247e2dffb892573cb5
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-08-29 10:55:44 -0300

    Change table name

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -13,9 +13,9 @@ import java.util.UUID;
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID>, JpaSpecificationExecutor<ModuleModel> {
 
 
-    @Query(value = "select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
+    @Query(value = "select * from modules where course_course_id = :courseId", nativeQuery = true)
     List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
-    @Query(value = "SELECT * FROM tb_modules WHERE course_course_id = :courseId AND module_id = :moduleId", nativeQuery = true)
+    @Query(value = "SELECT * FROM modules WHERE course_course_id = :courseId AND module_id = :moduleId", nativeQuery = true)
     Optional<ModuleModel> findModuleIntoCourse(@Param("courseId") UUID courseId, @Param("moduleId") UUID moduleId);
 
 
