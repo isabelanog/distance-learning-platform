8aec625f2cb3e981843f65dd0b0ec68157938478
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-10-31 11:07:40 -0300

    Creating async database + fix groupId

--- authuser/src/main/java/com/dlp/authuser/AuthUserApplication.java
+++ authuser/src/main/java/com/dlp/authuser/AuthUserApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class AuthuserApplication {
+public class AuthUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthuserApplication.class, args);
+		SpringApplication.run(AuthUserApplication.class, args);
 	}
 
 }
