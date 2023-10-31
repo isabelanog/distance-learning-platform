f84d276f19a9c8b8f7eb45580aca477d8962d9c1
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-10-31 11:21:37 -0300

    Workspace

--- course/src/main/java/com/ead/course/services/UtilService.java
+++
@@ -1,9 +0,0 @@
-package com.ead.course.services;
-
-import org.springframework.data.domain.Pageable;
-
-import java.util.UUID;
-
-public interface UtilService {
-    String createURLGetUsersByCourse(UUID courseId, Pageable pageable);
-}
