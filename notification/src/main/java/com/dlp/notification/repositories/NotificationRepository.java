2a21c533001391ff31f4e704cad5fa9875a3645c
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-12-04 15:23:49 -0300

    Implementing created + updated notification methods

--- notification/src/main/java/com/dlp/notification/repositories/NotificationRepository.java
+++ notification/src/main/java/com/dlp/notification/repositories/NotificationRepository.java
@@ -1,10 +1,17 @@
 package com.dlp.notification.repositories;
 
+import com.dlp.notification.enums.NotificationStatus;
 import com.dlp.notification.models.NotificationModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationModel, UUID> {
+
+    Page<NotificationModel> findAllByUserIdAndNotificationStatus(UUID userId, NotificationStatus notificationStatus, Pageable pageable);
+    Optional<NotificationModel> findNotificationModelByNotificationIdAndUserId(UUID notificationId, UUID userId);
 }
--- notification/src/main/java/com/dlp/notification/services/impl/NotificationServiceImpl.java
+++ notification/src/main/java/com/dlp/notification/services/impl/NotificationServiceImpl.java
@@ -1,9 +1,15 @@
 package com.dlp.notification.services.impl;
 
+import com.dlp.notification.enums.NotificationStatus;
 import com.dlp.notification.models.NotificationModel;
 import com.dlp.notification.repositories.NotificationRepository;
 import com.dlp.notification.services.NotificationService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
@@ -16,5 +22,15 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     public NotificationModel saveNotification(NotificationModel notificationModel) {
         return notificationRepository.save(notificationModel);
+    }
+
+    @Override
+    public Page<NotificationModel> getNotificationsCreatedByUserId(UUID userId, Pageable pageable) {
+        return notificationRepository.findAllByUserIdAndNotificationStatus(userId, NotificationStatus.CREATED, pageable);
+    }
+
+    @Override
+    public Optional<NotificationModel> getNotificationByNotificationIdAndUserId(UUID notificationId, UUID userId) {
+        return notificationRepository.findNotificationModelByNotificationIdAndUserId(notificationId, userId);
     }
 }
