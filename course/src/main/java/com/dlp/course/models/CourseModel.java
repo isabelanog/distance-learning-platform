1beda34378b0d3c9bdc4b6e7faa0e7e8ff3661d3
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-12-04 11:44:49 -0300

    Removing @referenceColumnName annotation in CourseModel

--- course/src/main/java/com/dlp/course/models/CourseModel.java
+++ course/src/main/java/com/dlp/course/models/CourseModel.java
@@ -64,8 +64,8 @@ public class CourseModel implements Serializable {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "COURSES_USERS",
-            joinColumns = @JoinColumn(name = "course_id", referencedColumnName = "courseId"),
-            inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "userId"))
+            joinColumns = @JoinColumn(name = "course_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
     private Set<UserModel> users;
 
     public UUID getCourseId() {
