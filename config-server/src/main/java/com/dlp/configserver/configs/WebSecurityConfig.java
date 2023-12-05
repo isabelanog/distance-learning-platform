0cbc1fca183b4b782a0aeae44bdbd54e0ea7ea01
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-12-05 14:15:34 -0300

    Implementing basic authentication in Config Server MS

---
+++ config-server/src/main/java/com/dlp/configserver/configs/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.dlp.configserver.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${dlp.configServer.username}")
+    private String username;
+
+    @Value("${dlp.configServer.password}")
+    private String password;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.httpBasic()
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser(username)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
