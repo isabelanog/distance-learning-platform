f52ef85395822e0c151b9fd89fa544effb06bd69
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-12-13 14:53:46 -0300

    Get username by JWT and validation

--- authuser/src/main/java/com/dlp/authuser/configs/security/JwtProvider.java
+++ authuser/src/main/java/com/dlp/authuser/configs/security/JwtProvider.java
@@ -1,7 +1,6 @@
 package com.dlp.authuser.configs.security;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
@@ -30,5 +29,28 @@ public class JwtProvider {
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
+    }
+
+    public String getUsernameByJwt(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwt(String authToken) {
+
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            log.error(" Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("JWT is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
     }
 }
