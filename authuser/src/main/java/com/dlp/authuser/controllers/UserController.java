dcc7aca1a53912e07335a7a3aa80e1e371b8b7aa
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-12-13 16:23:12 -0300

    Define authorization in controller layer

--- authuser/src/main/java/com/dlp/authuser/controllers/UserController.java
+++ authuser/src/main/java/com/dlp/authuser/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.dlp.authuser.controllers;
 
+import com.dlp.authuser.configs.security.AuthenticationCurrentUserService;
+import com.dlp.authuser.configs.security.UserDetailsImpl;
 import com.dlp.authuser.dtos.UserDto;
 import com.dlp.authuser.models.UserModel;
 import com.dlp.authuser.service.UserService;
 import com.dlp.authuser.specification.UserSpecificationTemplate;
 import com.fasterxml.jackson.annotation.JsonView;
 
+import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +34,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
+@Log4j2
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/users")
 public class UserController {
@@ -35,9 +43,17 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationCurrentUserService authenticationCurrentUserService;
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping
     public ResponseEntity<Page<UserModel>> getUsers(UserSpecificationTemplate.UserSpecification userSpecification,
-                                                    @PageableDefault(sort = "userId", direction = Sort.Direction.ASC) Pageable pageable) {
+                                                    @PageableDefault(sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
+                                                    Authentication authentication) {
+        UserDetails userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        log.info("Authentication {}", userDetails.getUsername());
+
         Page<UserModel> users = userService.getUsers(userSpecification, pageable);
         if (!users.isEmpty()) {
             for (UserModel user : users.toList()) {
@@ -47,16 +63,24 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(users);
     }
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/{userId}")
     public ResponseEntity<Object> getUserById(@PathVariable(value = "userId") UUID userId) {
-        Optional<UserModel> userModelOptional = userService.getUserById(userId);
 
-        if (userModelOptional.isEmpty()) {
-            logger.warn("User not found");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        UUID currentUserId = authenticationCurrentUserService.getCurrentUser().getUserId();
+
+        if (currentUserId.equals(userId)) {
+            Optional<UserModel> userModelOptional = userService.getUserById(userId);
+
+            if (userModelOptional.isEmpty()) {
+                logger.warn("User not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+            } else {
+                logger.info(userModelOptional.get().toString());
+                return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+            }
         } else {
-            logger.info(userModelOptional.get().toString());
-            return ResponseEntity.status(HttpStatus.OK).body(userModelOptional.get());
+            throw new AccessDeniedException("Forbidden");
         }
     }
 
