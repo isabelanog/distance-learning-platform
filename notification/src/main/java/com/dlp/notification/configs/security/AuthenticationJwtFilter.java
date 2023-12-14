9458460b2f8f27ab4325227b352ddd094a428efa
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-12-14 17:14:18 -0300

    Implement customized filter in authentication at Notification MS

---
+++ notification/src/main/java/com/dlp/notification/configs/security/AuthenticationJwtFilter.java
@@ -0,0 +1,62 @@
+package com.dlp.notification.configs.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+/*
+* This class has goal to implement a customized filter to validate JWT
+*/
+public class AuthenticationJwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try {
+            String tokenHeader = getTokenHeader(httpServletRequest);
+
+            if (tokenHeader != null && jwtProvider.validateJwt(tokenHeader)) {
+
+                String userId = jwtProvider.getJwtSubject(tokenHeader);
+
+                String roles = jwtProvider.getJwtClaimName(tokenHeader, "roles");
+
+                UserDetails userDetails = UserDetailsImpl.build(UUID.fromString(userId), roles);
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception e) {
+            logger.error("Cannot set user authentication: {}", e);
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private String getTokenHeader(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
+    }
+}
