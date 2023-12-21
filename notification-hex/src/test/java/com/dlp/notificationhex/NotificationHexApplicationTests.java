3fde00dffd944af1bff49ddd77ca7c203d346197
Isabela Nogueira <isabela.nogueirau@liferay.com>
2023-12-21 14:20:20 -0300

    Workspace

---
+++ notification-hex/src/test/java/com/dlp/notificationhex/NotificationHexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlp.notificationhex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationHexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
