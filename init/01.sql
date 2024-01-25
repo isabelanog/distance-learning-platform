8adc4c8a15ca873e6f70efec7bfa4e20851ccec0
Isabela Nogueira <isabela.nogueirau@liferay.com>
2024-01-25 14:50:35 -0300

    Making microservices ready to starts by docker compose

--- init/01.sql
+++ init/01.sql
@@ -1,6 +1,3 @@
 CREATE DATABASE IF NOT EXISTS `authuser`;
 CREATE DATABASE IF NOT EXISTS `course`;
 CREATE DATABASE IF NOT EXISTS `notification`;
-GRANT ALL ON `authuser`.* TO 'root'@'%';
-GRANT ALL ON `course`.* TO 'root'@'%';
-GRANT ALL ON `notification`.* TO 'root'@'%';
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build and start Docker containers
-docker-compose up -d
-
-# Wait for service-registry to be ready
-dockerize -wait tcp://service-registry:8761 -timeout 60s
-
